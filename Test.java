fc94906550ef7966450154c63741a97e1b77a00f
FDJ158 <janhvi.kshirsagar73@gmail.com>
2023-12-09 10:26:08 +0530

    janhviUpdate

--- Test.java
+++ Test.java
@@ -2,21 +2,10 @@ public class A
 {
 public Static void main(String [] args)
 {
-System.out.println("Hello");
-System.out.println("hi this git hub");
 
-System.out.println("new branch");
-System.out.println("Bhaiyya Saheb ");
-System.out.println("new branch")
-}
-public void Member1
-{
-    System.out.println("Hello Member");
-    System.out.println("Hello Member1 add");
-System.out.println("gajanan");
-}
-public void m1(){
-  System.out.println("vasanti");
+System.out.println("janhvi");
+
+
   
 }
 }
